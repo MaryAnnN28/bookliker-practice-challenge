3052b9f9c0ad4bb1d58902d31f9fc787b0e08c73
MaryAnnN28 <maryann.navarrete@gmail.com>
2021-04-03 16:47:49 -0400

    done

--- js/index.js
+++ js/index.js
@@ -1,52 +1,81 @@
-const booksURL = "http://localhost:3000/books"
+let like = true
 
-
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", () => {
   fetchBooks()
 });
 
-let books = []
-
 const fetchBooks = () => {
-  fetch(booksURL)
-    .then((resp) => resp.json())
-    .then((books) => {
-      books.forEach((book) => renderList(book))
-    })
+  fetch('http://localhost:3000/books')
+    .then(resp => resp.json())
+    .then(books => books.forEach(renderList))
 }
 
 const renderList = (book) => {
-  let bookList = document.querySelector('#list')
+  const bookList = document.getElementById('list')
 
   let bookObj = document.createElement('li')
       bookObj.innerText = book.title
-      bookObj.addEventListener("click", () => 
-        renderBook(book))
+      bookObj.addEventListener('click', () => renderBook(book))
 
-  bookList.append(bookObj)
-
+  bookList.appendChild(bookObj)
 }
 
 const renderBook = (book) => {
-  let bookImg = document.getElementById('book-img')
+  let bookContainer = document.getElementById("show-panel")
+      bookContainer.innerText = ""
+
+  let bookImg = document.createElement('img')
       bookImg.src = book.img_url
   
-  let bookTitle = document.getElementById('book-title')
-      bookTitle.innerText = book.title 
+  let bookTitle = document.createElement('h2')
+      bookTitle.innerText = book.title
   
-  let bookSubtitle = document.getElementById('book-subtitle')
-      bookSubtitle.innerText = book.subtitle
+  let subtitle = document.createElement('h4')
+      subtitle.innerText = book.subtitle
   
-  let bookAuthor = document.getElementById('book-author')
+  let bookAuthor = document.createElement('h4')
       bookAuthor.innerText = book.author
   
-  let bookDesc = document.getElementById('book-description')
-  bookDesc.innerText = book.description
+  let bookDesc = document.createElement('p')
+      bookDesc.innerText = book.description
   
-  let usersList = document.getElementById('users-list')
-    usersList.innerText = book.users.map(user => {
-   return `<li>${user.username}</li>`
-  })
-  return usersList
+  let usersList = document.createElement('ul')
+    
+    book.users.forEach(user => {
+        let li = document.createElement('li')
+        li.innerText = user.username
+      usersList.append(li)
+    })
   
+  let button = document.createElement('button')
+  like? button.innerText = "Like" : button.innerText = "Unlike"
+  button.addEventListener('click', () => likeBook(book, button))
+  
+  bookContainer.append(bookImg, bookTitle, subtitle, bookAuthor, bookDesc, usersList, button)
+  
+};
+
+const likeBook = (book) => {
+  let likeObj
+
+  if (like) {
+    const currentUser = { "id": 1, "username": "pouros" }
+    book.users.push(currentUser)
+    likeObj = { "users": book.users }
+    like = false
+  } else {
+    like = true
+    book.users.pop()
+    likeObj = {"users": book.users}
+  }
+
+  const reqPack = {
+    method: "PATCH",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(likeObj)
+  }
+
+  fetch(`http://localhost:3000/books/${book.id}`, reqPack)
+    .then(resp => resp.json())
+    .then(book => renderBook(book))
 }
