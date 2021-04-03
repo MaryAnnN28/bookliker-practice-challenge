893cab59ba899fe6cb2c8e629a6b41e86f4f6af2
MaryAnnN28 <maryann.navarrete@gmail.com>
2021-04-03 15:34:38 -0400

    update

--- js/index.js
+++ js/index.js
@@ -1,1 +1,52 @@
-document.addEventListener("DOMContentLoaded", function() {});
+const booksURL = "http://localhost:3000/books"
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  fetchBooks()
+});
+
+let books = []
+
+const fetchBooks = () => {
+  fetch(booksURL)
+    .then((resp) => resp.json())
+    .then((books) => {
+      books.forEach((book) => renderList(book))
+    })
+}
+
+const renderList = (book) => {
+  let bookList = document.querySelector('#list')
+
+  let bookObj = document.createElement('li')
+      bookObj.innerText = book.title
+      bookObj.addEventListener("click", () => 
+        renderBook(book))
+
+  bookList.append(bookObj)
+
+}
+
+const renderBook = (book) => {
+  let bookImg = document.getElementById('book-img')
+      bookImg.src = book.img_url
+  
+  let bookTitle = document.getElementById('book-title')
+      bookTitle.innerText = book.title 
+  
+  let bookSubtitle = document.getElementById('book-subtitle')
+      bookSubtitle.innerText = book.subtitle
+  
+  let bookAuthor = document.getElementById('book-author')
+      bookAuthor.innerText = book.author
+  
+  let bookDesc = document.getElementById('book-description')
+  bookDesc.innerText = book.description
+  
+  let usersList = document.getElementById('users-list')
+    usersList.innerText = book.users.map(user => {
+   return `<li>${user.username}</li>`
+  })
+  return usersList
+  
+}
